b233f6e81754dfd02eab6bebdbffa3c4ea119431
mrushidany <mrushidany@gmail.com>
2021-08-10 17:54:12 +0300

    Price Calculator

--- resources/views/manager/form/laundry_input_form.blade.php
+++ resources/views/manager/form/laundry_input_form.blade.php
@@ -1,7 +1,7 @@
         <div  class="card">
             <div class="card-body">
                 <div class="modal-header">
-                    <h5 class="modal-title offset-md-5">Add Laundry Details</h5>
+                    <h3 class="modal-title offset-md-5">Add Laundry Details</h3>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -11,11 +11,11 @@
                     @method('POST')
                     <div class="modal-body">
                         <div class="row">
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-3">
                                 <label for="client_name" class="control-label">Client Full Name</label>
                                 <input name="full_name" type="text" class="form-control">
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-3">
                                 <label for="input_type" class="control-label">Phone Number</label>
                                 <div class="input-group mb-4">
                                     <div class="input-group-prepend">
@@ -24,30 +24,59 @@
                                     <input type="text" class="form-control" placeholder="xxxxxxxxx" aria-label="Username" aria-describedby="basic-addon1">
                                 </div>
                             </div>
-                            <div class="form-group col-md-4">
-                                <label for="issued_by" class="control-label">Issued By</label>
-                                <select name="issued_by" class="form-control" >
-                                    <option></option>
-                                    <option value="Investor">Investor</option>
-                                    <option value="Government">Government</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-md-12">
-                                <label for="description">Description</label>
-                                <textarea class="form-control" rows="1" name="description"></textarea>
-                            </div>
 
+                            <div class="form-group col-md-6">
+                                <label for="washing_machine" class="control-label">Select Machine</label>
+                                <div class="input-group mb-4">
+                                    <div class="form-check form-check-inline form-control">
+                                        <input class="form-check-input" type="checkbox" id="washing_machine" value="">
+                                        <label class="form-check-label control-label" onclick="event.preventDefault()" for="washing_machine">Washing Machine</label>
+                                    </div>
+                                    <div class="form-check form-check-inline form-control">
+                                        <input class="form-check-input" type="checkbox" id="drying_machine" value="">
+                                        <label class="form-check-label control-label" for="drying_machine">Drying Machine</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="laundry_quantity">Laundry Quantity in Kg</label>
+                                <input type="number" min="8" step="8" class="form-control" id="laundry_quantity" value="">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="total_cost">Total Cost</label>
+                                <input type="text" class="form-control" id="total_cost" name="total_cost"  value="">
+                            </div>
+                            <div>
                         </div>
                     </div>
                     <div class="modal-footer">
                         <button type="reset" class="btn btn-warning">Reset</button>
                         <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Save </button>
                     </div>
-            </div>
                 </form>
+            </div>
         </div>
     </div>
+
+ <script type="application/javascript">
+     $(document).ready(function (){
+         var price = 4000;
+         $('#washing_machine').on('change', function (){
+             if ($(this).prop('checked', true)){
+                 $('#total_cost').val(total_cost(price));
+             }
+         });
+         $('#drying_machine').on('change', function (){
+             if ($(this).prop('checked', true)){
+                 $('#total_cost').val(total_cost(price));
+             }
+         });
+     });
+
+     function total_cost(price){
+        var total = price + ' ' + '/=';
+        return total
+     }
+ </script>
 
 
