66f5e9b547a1b468aa86dfdbfe5737d43daa1e95
mrushidany <mrushidany@gmail.com>
2021-09-07 12:28:34 +0300

    Pricing simply rectified

--- resources/views/home/landing_page.blade.php
+++ resources/views/home/landing_page.blade.php
@@ -256,53 +256,47 @@
         <div class="row">
             <div class="col-lg-4 mb-4">
                 <div class="bg-light text-center mb-2 pt-4">
-                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-secondary rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
-                        <h3 class="text-white">Basic</h3>
-                        <h1 class="display-4 text-white mb-0">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>49<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Mo</small>
+                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-easywash rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
+                        <h3 class="text-blue">Washing</h3>
+                        <h1 class="display-4 text-blue mb-0">
+                            <small class="align-top" style="font-size: 22px; line-height: 45px;">T<sub>shs</sub></small><small class="align-middle">4000</small><small class="align-bottom" style="font-size: 12px; line-height: 40px;">/ 8Kg</small>
                         </h1>
                     </div>
                     <div class="d-flex flex-column align-items-center py-3">
-                        <p>HTML5 & CSS3</p>
-                        <p>Bootstrap 4</p>
-                        <p>Responsive Layout</p>
-                        <p>Compatible With All Browsers</p>
+
+
                     </div>
-                    <a href="" class="btn btn-secondary py-2 px-4">Signup Now</a>
+                    <a href="" class="btn btn-secondary py-2 px-4">Book Now</a>
                 </div>
             </div>
             <div class="col-lg-4 mb-4">
                 <div class="bg-light text-center mb-2 pt-4">
-                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-primary rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
-                        <h3 class="text-white">Standard</h3>
-                        <h1 class="display-4 text-white mb-0">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>99<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Mo</small>
+                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-easywash rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
+                        <h3 class="text-blue">Drying</h3>
+                        <h1 class="display-4 text-blue mb-0">
+                            <small class="align-top" style="font-size: 22px; line-height: 45px;">T<sub>shs</sub></small><small class="align-middle">4000</small><small class="align-bottom" style="font-size: 12px; line-height: 40px;">/ 8Kg</small>
                         </h1>
                     </div>
                     <div class="d-flex flex-column align-items-center py-3">
-                        <p>HTML5 & CSS3</p>
-                        <p>Bootstrap 4</p>
-                        <p>Responsive Layout</p>
-                        <p>Compatible With All Browsers</p>
+
+
                     </div>
-                    <a href="" class="btn btn-primary py-2 px-4">Signup Now</a>
+                    <a href="" class="btn btn-primary py-2 px-4">Book Now</a>
                 </div>
             </div>
             <div class="col-lg-4 mb-4">
                 <div class="bg-light text-center mb-2 pt-4">
-                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-secondary rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
-                        <h3 class="text-white">Premium</h3>
-                        <h1 class="display-4 text-white mb-0">
-                            <small class="align-top" style="font-size: 22px; line-height: 45px;">$</small>149<small class="align-bottom" style="font-size: 16px; line-height: 40px;">/ Mo</small>
+                    <div class="d-inline-flex flex-column align-items-center justify-content-center bg-easywash rounded-circle shadow mt-2 mb-4" style="width: 200px; height: 200px; border: 15px solid #ffffff;">
+                        <h3 class="text-blue">Delivery</h3>
+                        <h1 class="display-4 text-blue mb-0">
+                            <small class="align-top" style="font-size: 22px; line-height: 45px;">T<sub>shs</sub></small><small class="align-middle">4000</small><small class="align-bottom" style="font-size: 12px; line-height: 40px;">/ 8Kg</small>
                         </h1>
                     </div>
                     <div class="d-flex flex-column align-items-center py-3">
-                        <p>HTML5 & CSS3</p>
-                        <p>Bootstrap 4</p>
-                        <p>Responsive Layout</p>
-                        <p>Compatible With All Browsers</p>
+
+
                     </div>
-                    <a href="" class="btn btn-secondary py-2 px-4">Signup Now</a>
+                    <a href="" class="btn btn-secondary py-2 px-4">Book Now</a>
                 </div>
             </div>
         </div>
