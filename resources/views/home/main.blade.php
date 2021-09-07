71f92a9affbf9ae84e58dc99513edebd64239b6c
mrushidany <mrushidany@gmail.com>
2021-09-07 09:59:47 +0300

    Adjancements in the landing page

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -38,10 +38,14 @@
                     <a class="text-white px-3" href="">
                         <i class="fab fa-facebook-f"></i>
                     </a>
+<<<<<<< HEAD
                     <a class="text-white px-3" href="">
                         <i class="fab fa-twitter"></i>
                     </a>
                     <a class="text-white px-3" href="">
+=======
+                    <a class="text-white px-3" href="https://instagram.com/easywashtz?utm_medium=copy_link">
+>>>>>>> f942894... Inserted the logo onto the landing page
                         <i class="fab fa-instagram"></i>
                     </a>
                 </div>
@@ -57,7 +61,7 @@
     <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 pl-3 pl-lg-5">
             <a href="" class="navbar-brand">
-                <h1 class="m-0 text-secondary"><span class="text-primary">Easy</span>Wash</h1>
+                <img  src="{{asset('assets/images/easywash_web_logo.jpeg')}}" />
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
