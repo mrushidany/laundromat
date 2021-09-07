26b7ee379f80517df586e8215b7a1e4caecaf366
mrushidany <mrushidany@gmail.com>
2021-09-07 12:33:50 +0300

    About us updated

--- resources/views/home/landing_page.blade.php
+++ resources/views/home/landing_page.blade.php
@@ -99,9 +99,9 @@
             </div>
             <div class="col-lg-7 mt-5 mt-lg-0 pl-lg-5">
                 <h6 class="text-secondary text-uppercase font-weight-medium mb-3">Learn About Us</h6>
-                <h1 class="mb-4">We Are Quality Laundry Provider In Your City</h1>
-                <h5 class="font-weight-medium font-italic mb-4">Clita sit et ipsum sed. Volup duo sea dolor rebum ea rebum kasd stet. Dolor at duo dolor sit stet.</h5>
-                <p class="mb-2">Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor</p>
+                <h1 class="mb-4">Laundry has never been Easier </h1>
+                <h5 class="font-weight-medium font-italic mb-4">We have redefined your laundry experience to make your least favorite chore more likable</h5>
+                <p class="mb-2">What got us started is the need to help people ease from domestic duties, and what keeps us going is the ability to do so every day while maintaining our customer’s smiles. Considering busy schedules, we understand the frustration a pile of laundry triggers after a long day. From washing to drying, folding, and having it delivered right at your doorsteps, our services are trusted to surpass expectations.</p>
                 <div class="row">
                     <div class="col-sm-6 pt-3">
                         <div class="d-flex align-items-center">
@@ -173,43 +173,8 @@
 
 
 <!-- Features Start -->
-{{--<div class="container-fluid py-5">--}}
-{{--    <div class="container">--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-lg-7 m-0 my-lg-5 pt-0 pt-lg-5 pr-lg-5">--}}
-{{--                <h6 class="text-secondary text-uppercase font-weight-medium mb-3">Our Features</h6>--}}
-{{--                <h1 class="mb-4">Why Choose Us</h1>--}}
-{{--                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor</p>--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-sm-6 mb-4">--}}
-{{--                        <h1 class="text-secondary" data-toggle="counter-up">10</h1>--}}
-{{--                        <h5 class="font-weight-bold">Years Expereince</h5>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-sm-6 mb-4">--}}
-{{--                        <h1 class="text-secondary" data-toggle="counter-up">250</h1>--}}
-{{--                        <h5 class="font-weight-bold">Expert Worker</h5>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-sm-6 mb-4">--}}
-{{--                        <h1 class="text-secondary" data-toggle="counter-up">1250</h1>--}}
-{{--                        <h5 class="font-weight-bold">Happy Clients</h5>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-sm-6 mb-4">--}}
-{{--                        <h1 class="text-secondary" data-toggle="counter-up">9550</h1>--}}
-{{--                        <h5 class="font-weight-bold">Dry Cleaning</h5>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-lg-5">--}}
-{{--                <div class="d-flex flex-column align-items-center justify-content-center bg-secondary h-100 py-5 px-3">--}}
-{{--                    <i class="fa fa-5x fa-certificate text-white mb-5"></i>--}}
-{{--                    <h1 class="display-1 text-white mb-3">10+</h1>--}}
-{{--                    <h1 class="text-white m-0">Years Experience</h1>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-<!-- Features End -->
+
+<!-- Features -->
 
 
 <!-- Working Process Start -->
