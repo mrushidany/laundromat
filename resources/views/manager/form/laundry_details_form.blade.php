53366bdf9cc73e7c25f486e4c50c60a497e5caa6
mrushidany <mrushidany@gmail.com>
2021-09-09 18:39:21 +0300

    Dynamic calculator in laundry details form for quantity and total cost

--- resources/views/manager/form/laundry_details_form.blade.php
+++ resources/views/manager/form/laundry_details_form.blade.php
@@ -25,7 +25,7 @@
                 <label for="washing_machine" >Select Machine</label>
                 <div class="input-group mb-4 select_machine">
                     <div class="form-check form-check-inline">
-                        <select id="multiple" class="js-states form-control" multiple style="width: 800px;">
+                        <select id="multiple" class="js-states form-control" name="machine_selected" multiple style="width: 800px;">
                             <option>Machine 1</option>
                             <option>Machine 2</option>
                             <option>Machine 3</option>
@@ -40,7 +40,7 @@
                     </div>
                     <div class="form-check form-check-inline was-validated">
                         <div class="custom-control custom-checkbox mb-3">
-                            <input type="checkbox" class="custom-control-input" id="customControlValidation1" required="">
+                            <input type="checkbox" name="drying_machine" class="custom-control-input" id="customControlValidation1" required="">
                             <label class="custom-control-label" for="customControlValidation1">Drying Machine</label>
                             <div class="invalid-feedback">Drying Machine Not Selected</div>
                         </div>
@@ -65,24 +65,28 @@
 
 <script type="application/javascript">
     $(document).ready(function (){
-        var number_of_machines,laundry_quantity;
+        var number_of_machines = 0, laundry_quantity = 0, total_cost = 0;
         $('select[name="machine_selected"]').on('change', function (){
-            number_of_machines = $(this).val().length
-            laundry_quantity = $('#laundry_quantity').val(laundry_quantity(8 * number_of_machines))
+            number_of_machines = $(this).val().length;
+            laundry_quantity = 8 * number_of_machines;
+            total_cost = number_of_machines * 4000;
+            $('#laundry_quantity').val(laundry_quantity + ' Kgs').trigger('change');
+            $('#total_cost').val(total_cost + ' /=').trigger('change');
+            return laundry_quantity, total_cost;
         })
         $('input[name="drying_machine"]').change(function (){
             if($(this).is(':checked')){
-                laundry_quantity = (8 * number_of_machines) * 2
-                console.log(laundry_quantity)
-            }else{
-                laundry_quantity = 8 * number_of_machines
-                console.log(laundry_quantity)
+              var total_quantity = laundry_quantity * 2;
+              var cost = total_cost * 2;
+              $('#laundry_quantity').val(total_quantity + ' Kgs').trigger('change');
+              $('#total_cost').val(cost + ' /=').trigger('change');
+            }else if (!$(this).is(':checked')){
+                total_quantity = laundry_quantity;
+                cost = total_cost;
+                $('#laundry_quantity').val(total_quantity + ' Kgs').trigger('change');
+                $('#total_cost').val(cost + ' /=').trigger('change');
+                console.log(total_quantity)
             }
         })
-
-        function laundry_quantity(quantity){
-            var total = quantity;
-            return total;
-        }
     })
 </script>
