4888f312238f09c36795e350b8edb269e53c55a5
mrushidany <mrushidany@gmail.com>
2021-09-13 12:14:01 +0300

    Created the InitialPaymentStatus model

---
+++ app/Models/InitialPaymentStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InitialPaymentStatus extends Model
+{
+    use HasFactory;
+}
