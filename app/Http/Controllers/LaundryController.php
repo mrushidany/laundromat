8afa672544a1c877d1b31d2b95daacc70439e419
mrushidany <mrushidany@gmail.com>
2021-10-28 19:24:05 +0300

    Update the cashdraw according to the date it was updated

--- app/Http/Controllers/LaundryController.php
+++ app/Http/Controllers/LaundryController.php
@@ -220,12 +220,12 @@ class LaundryController extends Controller
                         ->join("laundry_costs",'laundry_costs.laundry_details_id','=','laundry_details.id')
                         ->whereDate('laundry_details.created_at', Carbon::yesterday()->format('Y-m-d') )
                         ->get(['laundry_details.id','routine_clients.full_name','routine_clients.phone','laundry_details.selected_machines','laundry_details.quantity','laundry_costs.amount','laundry_details.created_at','laundry_costs.payment_status','laundry_costs.id as laundry_cost_id']);
-                    $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('created_at', Carbon::yesterday()->format('Y-m-d') )->sum('amount');
-                    $cost_to_be_paid = LaundryCost::where('payment_status','!=',null)->whereDate('created_at', Carbon::yesterday()->format('Y-m-d') )->sum('amount');
+                    $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('updated_at', Carbon::yesterday()->format('Y-m-d') )->sum('amount');
+                    $cost_to_be_paid = LaundryCost::where('payment_status','!=',null)->whereDate('updated_at', Carbon::yesterday()->format('Y-m-d') )->sum('amount');
                     $partial_payments = LaundryCost::join('initial_payments','initial_payments.laundry_cost_id','=','laundry_costs.id')
                         ->where('laundry_cost_id','!=',null)
                         ->where('laundry_costs.payment_status','=','Partial Payment')
-                        ->whereDate('initial_payments.created_at', Carbon::yesterday()->format('Y-m-d') )
+                        ->whereDate('initial_payments.updated_at', Carbon::yesterday()->format('Y-m-d') )
                         ->sum('initial_payment');
                     $cash_draw = $paid_laundry_cost + $partial_payments;
                     //Today filter triggered
@@ -234,12 +234,12 @@ class LaundryController extends Controller
                         ->join("laundry_costs",'laundry_costs.laundry_details_id','=','laundry_details.id')
                         ->whereDate('laundry_details.created_at', Carbon::today()->format('Y-m-d') )
                         ->get(['laundry_details.id','routine_clients.full_name','routine_clients.phone','laundry_details.selected_machines','laundry_details.quantity','laundry_costs.amount','laundry_details.created_at','laundry_costs.payment_status','laundry_costs.id as laundry_cost_id']);
-                    $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('created_at', Carbon::today()->format('Y-m-d') )->sum('amount');
-                    $cost_to_be_paid = LaundryCost::where('payment_status','!=', null)->whereDate('created_at', Carbon::today()->format('Y-m-d') )->sum('amount');
+                    $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('updated_at', Carbon::today()->format('Y-m-d') )->sum('amount');
+                    $cost_to_be_paid = LaundryCost::where('payment_status','!=', null)->whereDate('updated_at', Carbon::today()->format('Y-m-d') )->sum('amount');
                     $partial_payments = LaundryCost::join('initial_payments','initial_payments.laundry_cost_id','=','laundry_costs.id')
                         ->where('laundry_cost_id','!=',null)
                         ->where('laundry_costs.payment_status','=','Partial Payment')
-                        ->whereDate('initial_payments.created_at', Carbon::today()->format('Y-m-d') )
+                        ->whereDate('initial_payments.updated_at', Carbon::today()->format('Y-m-d') )
                         ->sum('initial_payment');
                     $cash_draw = $paid_laundry_cost + $partial_payments;
                 }
@@ -287,12 +287,12 @@ class LaundryController extends Controller
                     ->join("laundry_costs",'laundry_costs.laundry_details_id','=','laundry_details.id')
                     ->whereDate('laundry_details.created_at', Carbon::today()->format('Y-m-d') )
                     ->get(['laundry_details.id','routine_clients.full_name','routine_clients.phone','laundry_details.selected_machines','laundry_details.quantity','laundry_costs.amount','laundry_details.created_at','laundry_costs.payment_status','laundry_costs.id as laundry_cost_id']);
-                $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('created_at', Carbon::today()->format('Y-m-d') )->sum('amount');
-                $cost_to_be_paid = LaundryCost::where('payment_status','!=', null)->whereDate('created_at', Carbon::today()->format('Y-m-d') )->sum('amount');
+                $paid_laundry_cost = LaundryCost::where('payment_status','Paid')->whereDate('updated_at', Carbon::today()->format('Y-m-d') )->sum('amount');
+                $cost_to_be_paid = LaundryCost::where('payment_status','!=', null)->whereDate('updated_at', Carbon::today()->format('Y-m-d') )->sum('amount');
                 $partial_payments = LaundryCost::join('initial_payments','initial_payments.laundry_cost_id','=','laundry_costs.id')
                     ->where('laundry_cost_id','!=',null)
                     ->where('laundry_costs.payment_status','=','Partial Payment')
-                    ->whereDate('initial_payments.created_at', Carbon::today()->format('Y-m-d') )
+                    ->whereDate('initial_payments.updated_at', Carbon::today()->format('Y-m-d') )
                     ->sum('initial_payment');
                 $cash_draw = $paid_laundry_cost + $partial_payments;
             }
@@ -359,10 +359,8 @@ class LaundryController extends Controller
 
                 }
 
-
                 $data = ['type' => 'success', 'title' => 'Successful', 'text' => 'Payment Status updated successful'];
                 return \Request::ajax() ? response()->json($data) : redirect()->back()->with('data', $data);
-
 
         }
 
